817533507534f66fa5fc189e3d2fb887a2b19d7d
Radis Valiullin <radisvaliullin@gmail.com>
2018-05-14 02:48:46 +0300

    gcp: billingapi: explore: rest api: proj & services

--- envconf/example.setenv.sh
+++ envconf/example.setenv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 export GOOGLE_APPLICATION_CREDENTIALS=yourPath
+export GOOGLE_APP_API_KEY=yourAPIKey
 # 
 export APP_PROJECT_ID=yourProjID
 export APP_PROJECT_NEWBUCKET=yourProjNewBucketName
